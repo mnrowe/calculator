270a92c0246e4f0d781383c0b70f0040a84c3ea2
Matthew Rowe <mrowe.w@gmail.com>
2018-02-17 15:35:11 -0500

    Change state in app to a singular peice of state to caluclate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstOperand: '',
+      operation: '',
       layout: [7, 8, 9, '%', 4, 5, 6, 'x', 1, 2, 3, '-', 0, '.', '+', '=', 'AC']
     };
   }
@@ -16,30 +16,39 @@ class App extends Component {
     const numbers = this.state.layout.filter(num => typeof num === 'number');
     const operators = this.state.layout.filter(op => typeof op === 'string');
 
-    if (value === 'AC') {
-      this.setState({ firstOperand: '' });
-    } else {
-      switch (value) {
-        case '7':
-        case '8':
-        case '9':
-        case '4':
-        case '5':
-        case '6':
-        case '1':
-        case '2':
-        case '3':
-        case '0':
-          this.setState(prevState => ({
-            firstOperand: prevState.firstOperand + value
-          }));
-      }
+    switch (value) {
+      case 'AC':
+        this.setState({ operation: '' });
+        break;
+      case '7':
+      case '8':
+      case '9':
+      case '4':
+      case '5':
+      case '6':
+      case '1':
+      case '2':
+      case '3':
+      case '0':
+        this.setState(prevState => ({
+          operation: prevState.operation + value
+        }));
+        break;
+      case '.':
+      case 'x':
+      case '-':
+      case '%':
+      case '+':
+        this.setState(prevState => ({
+          operation: prevState.operation + ` ${value} `
+        }));
+        break;
     }
   }
   render() {
     return (
       <div className="App">
-        <Display value={this.state.firstOperand} />
+        <Display operation={this.state.operation} />
         <Frame
           layout={this.state.layout}
           onClick={event => this.handleButtonClick(event)}
