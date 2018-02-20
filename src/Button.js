ce2aaf1003e27fccfe4ec5a4d5d2c58a0a776e93
Matthew Rowe <mrowe.w@gmail.com>
2018-02-19 20:59:24 -0500

    make Button component easier to understand

--- src/Button.js
+++ src/Button.js
@@ -2,24 +2,11 @@ import React, { Component } from 'react';
 import './Button.css';
 
 class Button extends Component {
-  renderButton() {
-    const { value } = this.props;
-
-    return (
-      <button
-        value={this.props.value}
-        onClick={this.props.onClick}
-        className={'Button'}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-
   render() {
-    {
-      return this.renderButton();
-    }
+    const { className, onPress, ...props } = this.props;
+    return (
+      <button className={`Button ${className}`} onClick={onPress} {...props} />
+    );
   }
 }
 
