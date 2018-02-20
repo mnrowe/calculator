4ad29fc47c4e6ec689920a42e9881cab7f449d3b
Matthew Rowe <mrowe.w@gmail.com>
2018-02-19 19:34:12 -0500

    refactor App component to make it easier to use methods

--- src/App.js
+++ src/App.js
@@ -8,56 +8,38 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      operation: '',
-      total: '',
-      lastOperator: '',
-      layout: [7, 8, 9, '%', 4, 5, 6, 'x', 1, 2, 3, '-', 0, '.', '+', '=', 'AC']
+      value: null,
+      displayValue: '0',
+      operator: null,
+      waitingForOperand: false
     };
-  }
-  handleButtonClick(event) {
-    const { value } = event.target;
-    const { lastOperator } = this.state;
 
-    debugger;
-    switch (value) {
-      case 'AC':
-        this.setState({ operation: '' });
-        break;
-      case '7':
-      case '8':
-      case '9':
-      case '4':
-      case '5':
-      case '6':
-      case '1':
-      case '2':
-      case '3':
-      case '0':
-        this.setState(prevState => ({
-          operation: prevState.operation + value
-        }));
-        break;
-      case '.' && lastOperator !== '.':
-      case 'x':
-      case '-':
-      case '%':
-      case '+' && lastOperator !== '+':
-        this.setState(prevState => ({
-          operation: prevState.operation + ` ${value} `,
-          lastOperator: value
-        }));
-      case '=':
-        calculate(this.state.operation);
+    this.inputDigit = this.inputDigit.bind(this);
+  }
+
+  inputDigit(digit) {
+    const { displayValue, waitingForOperand } = this.state;
+
+    if (waitingForOperand) {
+      this.setState({
+        displayValue: String(digit),
+        waitingForOperand: false
+      });
+    } else {
+      this.setState({
+        displayValue:
+          displayValue === '0' ? String(digit) : displayValue + digit
+      });
     }
   }
+
   render() {
     return (
       <div className="App">
         <Display operation={this.state.operation} />
         <Frame
-          layout={this.state.layout}
+          inputDigit={this.inputDigit}
           onClick={event => this.handleButtonClick(event)}
-          isClear={this.state.isClear}
         />
       </div>
     );
