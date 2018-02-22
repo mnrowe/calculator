c439f2fad115e91764c97353582c09b6c6d9f706
Matthew Rowe <mrowe.w@gmail.com>
2018-02-21 21:08:39 -0500

    rename all Button component to Key component

--- src/Frame.js
+++ src/Frame.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Frame.css';
-import Button from './Button';
+import Key from './Key';
 
 const Frame = ({
   handleInputDigit,
@@ -10,65 +10,69 @@ const Frame = ({
   return (
     <div className="Frame">
       <div className="frame-row">
-        <Button className="button-clear" onClick={() => handleInputClearAll()}>
+        <Key
+          color="blue"
+          className="button-clear"
+          onClick={() => handleInputClearAll()}
+        >
           AC
-        </Button>
+        </Key>
       </div>
       <div className="frame-row">
-        <Button className="button-7" onClick={() => handleInputDigit(7)}>
+        <Key className="button-7" onClick={() => handleInputDigit(7)}>
           7
-        </Button>
-        <Button className="button-8" onClick={() => handleInputDigit(8)}>
+        </Key>
+        <Key className="button-8" onClick={() => handleInputDigit(8)}>
           8
-        </Button>
-        <Button className="button-9" onClick={() => handleInputDigit(9)}>
+        </Key>
+        <Key className="button-9" onClick={() => handleInputDigit(9)}>
           9
-        </Button>
-        <Button className="button-%" onClick={() => handleInputOperator('%')}>
+        </Key>
+        <Key className="button-%" onClick={() => handleInputOperator('%')}>
           %
-        </Button>
+        </Key>
       </div>
       <div className="frame-row">
-        <Button className="button-4" onClick={() => handleInputDigit(4)}>
+        <Key className="button-4" onClick={() => handleInputDigit(4)}>
           4
-        </Button>
-        <Button className="button-5" onClick={() => handleInputDigit(5)}>
+        </Key>
+        <Key className="button-5" onClick={() => handleInputDigit(5)}>
           5
-        </Button>
-        <Button className="button-6" onClick={() => handleInputDigit(7)}>
+        </Key>
+        <Key className="button-6" onClick={() => handleInputDigit(7)}>
           6
-        </Button>
-        <Button className="button-*" onClick={() => handleInputOperator('*')}>
+        </Key>
+        <Key className="button-*" onClick={() => handleInputOperator('*')}>
           *
-        </Button>
+        </Key>
       </div>
       <div className="frame-row">
-        <Button className="button-1" onClick={() => handleInputDigit(1)}>
+        <Key className="button-1" onClick={() => handleInputDigit(1)}>
           1
-        </Button>
-        <Button className="button-2" onClick={() => handleInputDigit(2)}>
+        </Key>
+        <Key className="button-2" onClick={() => handleInputDigit(2)}>
           2
-        </Button>
-        <Button className="button-3" onClick={() => handleInputDigit(3)}>
+        </Key>
+        <Key className="button-3" onClick={() => handleInputDigit(3)}>
           3
-        </Button>
-        <Button className="button--" onClick={() => handleInputOperator('-')}>
+        </Key>
+        <Key className="button--" onClick={() => handleInputOperator('-')}>
           -
-        </Button>
+        </Key>
       </div>
       <div className="frame-row">
-        <Button className="button-0" onClick={() => handleInputDigit(0)}>
+        <Key className="button-0" onClick={() => handleInputDigit(0)}>
           0
-        </Button>
-        <Button className="button--" onClick={() => handleInputOperator('.')}>
+        </Key>
+        <Key className="button--" onClick={() => handleInputOperator('.')}>
           .
-        </Button>
-        <Button className="button--" onClick={() => handleInputOperator('=')}>
+        </Key>
+        <Key className="button--" onClick={() => handleInputOperator('=')}>
           =
-        </Button>
-        <Button className="button--" onClick={() => handleInputOperator('+')}>
+        </Key>
+        <Key className="button--" onClick={() => handleInputOperator('+')}>
           +
-        </Button>
+        </Key>
       </div>
     </div>
   );
